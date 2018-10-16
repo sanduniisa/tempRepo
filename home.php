4c5710bed02c2b607801fbada321d3cd778940c3
sanduniisa <30472448+sanduniisa@users.noreply.github.com>
2018-10-16 15:06:44 +0530

    add my self

---
+++ home.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Ko-Bass</title>
+	<link rel="icon" href="images/dskuhdks.jpg">
+	<style>
+		/*style for header*/ .header {
+			background-color: #000000;
+			font-size: 20px; 
+			overflow: auto;
+  			padding: 10px;
+		}
+		.header-right{
+			float: right;
+			outline-color: #CD853F;
+			font-size: 30px;
+		}
+
+		.button{
+			background-color:  #ffae19;
+    		border: none;
+    		color: black;
+    		padding: 10px;
+    		text-align: center;
+    		text-decoration: none;
+    		display: inline-block;
+    		font-size: 16px;
+   			margin: 4px 2px;
+   			cursor: pointer;
+   			border-radius: 8px;
+   		}
+
+   		.footer {
+   			
+  			left: 0;
+   			bottom: 0;
+   			width: 100%;
+   			background-color: #000000;
+   			color: #CD853F;
+   			text-align: center;
+}
+
+		
+	</style>
+</head>
+<body>
+	<header class="header"><img src="images/dskuhdks.jpg"align="left">
+		<div class="header-right">
+    	<button class="button">Home</button>
+    	<button class="button">About Us</button>
+    	<button class="button">Contact Us</button>
+    	<button class="button">Help</button>
+    	<button class="button">Sign In</button>
+    	<button class="button">Sign Up</button>
+ 		</div>
+ 	</header>
+    
+    <p style="text-align:justify">Find Lanka offers incident based services for Home Solution, Business Solution and Enterprise Solution.
+										Our delivery has remote teach support. It offers a comprehensive suite of service offering including optimization, diagnosis and repair, setup & install, virus removal, usability training, networking and managed services.
+										Customers can choose from various channels to contact us, including web based support, real time phone, chat and e-mail or ground technicions.
+										The customer satisfaction scores of Find Lanka are amongest highest in the technical support industry.</p>
+
+
+
+   <h1>Our Goal</h1>
+								<br>
+								<p>To build good reputation in the field of real estate and property management and become a key player in the industry.</p>
+								<br>
+
+								<h1>Our Vision</h1>
+								<br>
+								<p>To provide quality services that exceeds the expectations of our esteemed customers.</p>
+								<br>
+
+								<h1>Our Mission</h1>
+								<br>
+								<p>To built long term relationship with our customers and provide exceptional customer services by innovation and advance technology.</p>
+								<br>
+
+								<h1>Core Value</h1>
+								<br>
+								<p>We believe in treating our customers with respect and faith. We grow through creativity invention and innovation.</p>
+ 	<footer class="stickyfooter">
+			<p class="footer"> &copy; KoBass.lk. 2018 | All Rights Reserved.</p>
+			
+	</footer>
+	
+ 
+</body>
+</html>
